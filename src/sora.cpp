413ba309cceeb14e9ad8ef0b78416dd956519fb2
melpon <shigemasa7watanabe+github@gmail.com>
2024-07-24 18:26:22 +0900

    User-Agent を設定する

--- src/sora.cpp
+++ src/sora.cpp
@@ -484,6 +484,8 @@ void Sora::DoConnect(const sora_conf::internal::ConnectConfig& cc,
         sora_context_->signaling_thread()->BlockingCall([this]() {
           return sora_context_->connection_context()->default_socket_factory();
         });
+    config.user_agent = boost::optional<std::string>(
+        "Mozilla 5.0 (Sora Unity SDK/" SORA_UNITY_SDK_VERSION ")");
 
     signaling_ = sora::SoraSignaling::Create(std::move(config));
     signaling_->Connect();
